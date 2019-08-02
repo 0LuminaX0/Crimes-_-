10989569701c3defe5322a1555c2d00b1d375b08
Andrii Negrub <zurg0505@gmail.com>
2019-08-02 16:34:19 +0300

    added base

--- The crimes/Task_1.py
+++ The crimes/Task_1.py
@@ -10,4 +10,32 @@
 10 баллов
 10 минут
 """
+from json import dumps
 
+
+crimes = {}
+
+with open("Crimes.csv", "rt") as text:
+    lines = text.readlines()
+first = lines.pop(0).strip().split(",")
+
+for line in lines:
+    line = line.strip("\n")
+    lin = line.split(",")
+    if lin[5] not in crimes:
+        crimes[lin[5]] = []
+    crimes[lin[5]].append({
+        first[0]: int(lin[0]),
+        first[1]: lin[1],
+        first[6]: lin[6],
+        first[7]: True if lin[7] == "True" else False,
+        "Date": {
+            first[2]: int(lin[2]),
+            first[3]: int(lin[3]),
+            first[4]: int(lin[4])
+        },
+        first[8]: lin[8]
+        })
+
+
+print(dumps(crimes, indent=4))
