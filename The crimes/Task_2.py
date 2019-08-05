0e5fe88853a8d6fae4bc6f18ab93fbfe71ae5729
Andrii Negrub <zurg0505@gmail.com>
2019-08-05 14:10:26 +0300

    task2

--- The crimes/Task_2.py
+++ The crimes/Task_2.py
@@ -2,7 +2,7 @@ from  Task_1 import crimes
 ans = int(input("Year: "))
 print(ans)
 for name in crimes:
-    for data in crimes[name]:
-        if ans == [data]["Date"]["Year"]:
+    for dat in crimes[name]:
+        if ans == dat["Date"]["Year"]:
             print(name)
 
